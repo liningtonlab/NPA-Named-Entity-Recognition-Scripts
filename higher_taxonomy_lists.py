1394ac95db914e91af11dbefd084b1fb58197946
A-Maras <andrew_maras@sfu.ca>
2021-07-15 09:43:47 -0700

    Higher Taxonomy Lists
    Generation and addition of genera lists for Animals, Plants and Microbes

---
+++ higher_taxonomy_lists.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+
+def main():
+    db_connection = sqlite3.connect("ITIS.sqlite")
+    curse = db_connection.cursor()
+
+    # Animal Genera List Generation
+    animal_rows = curse.execute(
+            "SELECT * FROM taxonomic_units WHERE kingdom_id IS 5"
+        )
+    animal_genera = []
+    for animal in animal_rows:
+        animal_genera.append(animal[2])
+    unique_animal_genera = list(set(animal_genera))
+    with open("animal_genera.txt", "w", encoding="utf-8") as f_one:
+        f_one.write(str(sorted(unique_animal_genera)))
+
+    # Plant Genera List Generation
+    plant_rows = curse.execute(
+            "SELECT * FROM taxonomic_units WHERE kingdom_id IS 3"
+        )
+    plant_genera = []
+    for plant in plant_rows:
+        plant_genera.append(plant[2])
+    unique_plant_genera = list(set(plant_genera))
+    with open("plant_genera.txt", "w", encoding="utf-8") as f_two:
+        f_two.write(str(sorted(unique_plant_genera)))
+
+    # Microbe Genera List Generation
+    microbe_rows = curse.execute(
+            "SELECT * FROM taxonomic_units WHERE kingdom_id IS 1 OR kingdom_id IS 2 OR kingdom_id IS 4 OR kingdom_id IS 6 OR kingdom_id IS 7"
+        )
+    microbe_genera = []
+    for microbe in microbe_rows:
+        microbe_genera.append(microbe[2])
+    unique_microbe_genera = list(set(microbe_genera))
+    with open("microbe_genera.txt", "w", encoding="utf-8") as f_third:
+        f_third.write(str(sorted(unique_microbe_genera)))
+
+
+if __name__ == "__main__":
+    main()
